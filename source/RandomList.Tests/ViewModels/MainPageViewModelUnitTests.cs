d9eaa52d2fd512df9cb2e87225ff3b926aed9ebf
Corey Baron <corey.baron@gmail.com>
2018-01-18 14:30:21 -0600

    initial passing test added

---
+++ source/RandomList.Tests/ViewModels/MainPageViewModelUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Prism.Navigation;
+using RandomList.ViewModels;
+
+namespace RandomList.Tests.ViewModels
+{
+    [TestClass]
+    public class MainPageViewModelUnitTests
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ShouldConstructMainPageViewModel()
+        {
+            // arrange
+            var navigation = Substitute.For<INavigationService>();
+
+            // act
+            var mainPageViewModel = new MainPageViewModel(navigation);
+
+            // assert
+            mainPageViewModel.Should().NotBeNull();
+        }
+    }
+}
