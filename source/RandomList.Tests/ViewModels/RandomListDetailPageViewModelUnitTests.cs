ef2395f4205bbd13062a6662bbfaad57fa5df099
Corey Baron <corey.baron@gmail.com>
2018-01-22 17:32:04 -0600

    test improvement for coverage

---
+++ source/RandomList.Tests/ViewModels/RandomListDetailPageViewModelUnitTests.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RandomListDetailPageViewModelUnitTests.cs" company="Corey Baron">
+// Copyright (c) Corey Baron. All Rights Reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RandomList.Tests.ViewModels
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NSubstitute;
+    using Prism.Navigation;
+    using RandomList.ViewModels;
+
+    [TestClass]
+    public class RandomListDetailPageViewModelUnitTests
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ShouldConstructRandomListDetailPageViewModel()
+        {
+            // arrange
+            var navigation = Substitute.For<INavigationService>();
+
+            // act
+            var randomListDetailPageViewModel = new RandomListDetailPageViewModel(navigation);
+
+            // assert
+            randomListDetailPageViewModel.Should().NotBeNull();
+        }
+    }
+}
